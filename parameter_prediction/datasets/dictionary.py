c277420a287d87468d0e0c4af91661c7ca240bae
Misha Denil <misha.denil@gmail.com>
2013-10-15 18:49:05 -0700

    Add GaussianKernelDictionary.

--- parameter_prediction/datasets/dictionary.py
+++ parameter_prediction/datasets/dictionary.py
@@ -41,6 +41,46 @@ class ExplicitDictionary(object):
     def size(self):
         return self.W.shape[0]
 
+
+
+def enumerate_space(extent):
+    """Enumerate all the points in a space with the given extent.  Use this
+    function to ensure spaces are always enumerated in the same order.
+    """
+    return itertools.imap(np.asarray, itertools.product(*map(xrange, extent)))
+
+
+class GaussianKernelDictionary(object):
+    def __init__(self, extent, scale):
+        try:
+            iter(scale)
+            scale_iterable = True
+        except TypeError:
+            scale_iterable = False
+
+        if scale_iterable:
+            assert len(extent) == len(scale)
+        else:
+            scale = [scale]*len(extent)
+
+        self.scale = np.asarray(scale).reshape((-1,1))
+        self.extent = np.asarray(extent)
+        self.__points = np.vstack(enumerate_space(self.extent)).T
+
+    def get_subdictionary(self, indices):
+        return np.vstack([self.get_atom(index) for index in indices])
+
+    def get_atom(self, index):
+        point = np.atleast_2d(self.__points[:,index]).T
+        E = np.sum((self.__points - point)**2 / self.scale**2, axis=0)
+        atom = np.exp(-0.5 * E)
+        return atom 
+
+    @property
+    def input_dim(self):
+        return np.prod(self.extent)
+
     @property
     def size(self):
-        return self.W.shape[1]
+        return np.prod(self.extent)
+
