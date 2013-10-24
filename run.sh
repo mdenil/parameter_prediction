fe7c719e1d58303386f1dd20703e1113a6509f01
Misha Denil <misha.denil@gmail.com>
2013-10-23 17:20:57 -0700

    Replace train.sh with a more general run.sh for running scripts.

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 tool_script.py arg0 arg1 ..."
+    exit 1;
+fi
+
+HERE=$(dirname $(readlink -e $0))
+TOOLPATH="$HERE/tools:$HERE/external/pylearn2/pylearn2/scripts"
+export PYTHONPATH="$HERE"
+source activate "$HERE/pp_env"
+
+TOOL_NAME="$1";
+TOOL_PARAMS="${@:2}"
+
+for D in ${TOOLPATH//:/ }; do
+    if [ -f "$D/$TOOL_NAME" ]; then
+        TOOL="$D/$TOOL_NAME"
+        break
+    fi
+done
+
+echo "Running: '$TOOL'"
+echo "Params : $TOOL_PARAMS"
+
+python "$TOOL" $TOOL_PARAMS
--- train.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-HERE=$(dirname $(readlink -e $0))
-
-export PYTHONPATH="$HERE"
-source activate "$HERE/pp_env"
-
-python external/pylearn2/pylearn2/scripts/train.py "$@"
