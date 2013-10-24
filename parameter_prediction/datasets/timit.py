28e5cf296def5decb82a7d41cd51d711ae6e0a04
Misha Denil <misha.denil@gmail.com>
2013-10-24 14:33:05 -0700

    Add sentence IDs to TIMIT dataset.

--- parameter_prediction/datasets/timit.py
+++ parameter_prediction/datasets/timit.py
@@ -10,54 +10,62 @@ class TIMIT(dense_design_matrix.DenseDesignMatrix):
         """
 
         if which_set == "train":
-            X, Y = self._load_train()
+            X, Y, sentence_ids = self._load_train()
 
         elif which_set == "test":
-            X, Y = self._load_test()
+            X, Y, sentence_ids = self._load_test()
 
         elif which_set == "valid":
-            X, Y = self._load_valid()
+            X, Y, sentence_ids = self._load_valid()
 
         elif which_set == "test_valid":
-            X1, Y1 = self._load_test()
-            X2, Y3 = self._load_valid()
+            X1, Y1, sentence_ids_1 = self._load_test()
+            X2, Y2, sentence_ids_2 = self._load_valid()
+
+            sentence_ids_2 += 1 + sentence_ids_1.max()
 
             X = np.concatenate([X1, X2], axis=0)
             Y = np.concatenate([Y1, Y2], axis=0)
+            sentence_ids = np.concatenate([sentence_ids_1, sentence_ids_2])
 
         else:
             raise Exception("TIMIT doesn't understand which_set='{}'".format(which_set))
 
         super(TIMIT,self).__init__(X=X, y=Y, axes=('b', 0))
+        self.sentence_ids = sentence_ids
 
         assert not np.any(np.isnan(self.X))
         assert not np.any(np.isnan(self.y))
+        assert not np.any(np.isnan(self.sentence_ids))
 
         if preprocessor:
             preprocessor.apply(self)
 
     def _load_file(self, name):
         data = np.load(name)
-        return data['X'], data['Y']
+        return data['X'], data['Y'], data['sentence_ids'].ravel()
 
     def _load_train(self):
         n_batches = 5
 
         Xs = []
         Ys = []
+        sentence_ids = []
         for b in xrange(1, n_batches+1):
             fname = os.path.join(
                     string_utils.preprocess("${PYLEARN2_DATA_PATH}"),
                     "timit",
                     "timit_train_b" + str(b) + ".npz")
-            X, Y = self._load_file(fname)
+            X, Y, ids = self._load_file(fname)
             Xs.append(X)
             Ys.append(Y)
+            sentence_ids.append(ids)
 
         X = np.concatenate(Xs, axis=0)
         Y = np.concatenate(Ys, axis=0)
+        sentence_ids = np.concatenate(sentence_ids)
 
-        return X, Y
+        return X, Y, sentence_ids
 
     def _load_test(self):
         fname = os.path.join(
