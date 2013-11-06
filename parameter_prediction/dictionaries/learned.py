bcb342f5375636cbfdb3827d4c3ea55e227f6815
bshakibi <bshakibi@gmail.com>
2013-11-05 18:51:14 -0800

    Added covariance dictionary.

--- parameter_prediction/dictionaries/learned.py
+++ parameter_prediction/dictionaries/learned.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from parameter_prediction.dictionaries.explicit import ExplicitDictionary
 
 class AutoencoderDictionary(ExplicitDictionary):
@@ -21,4 +23,22 @@ class AutoencoderDictionary(ExplicitDictionary):
         assert W.shape == (ae_model.nhid, ae_model.input_space.dim)
 
         return W
+
+class CovarianceDictionary(ExplicitDictionary):
+    """
+    Creates a dictionary using empirical covariance of the input data.
+    """
+    def __init__(self, inputs):
+        """
+        inputs : Dataset or TransformerDataset.
+        """
+        self.W = np.cov(self.get_data(inputs).T)
+
+    def get_data(self, inputs):
+        from pylearn2.datasets.transformer_dataset import TransformerDataset
+        if isinstance(inputs, TransformerDataset):
+            X = inputs.transformer.perform(inputs.raw)
+        else:
+            X = inputs.X
+        return X
 
